c49da1f286f3f2a4fa2f58bb1e657524ff4a8e16
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-09 12:00:26 -0700

    pulling Max update

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS potluck_db;
+CREATE DATABASE potluck_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE potluck_db;
