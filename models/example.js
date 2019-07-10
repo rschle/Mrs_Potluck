c272271617cc07959efc5652309b02dda8458f10
Oakkar <oakkaraung1659@gmail.com>
2019-07-10 11:35:47 -0700

    testing

--- models/example.js
+++ models/example.js
@@ -1,16 +1,16 @@
-module.exports = (sequelize, DataTypes) => {
-  const Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
+// module.exports = (sequelize, DataTypes) => {
+//   const Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
 
-  Example.associate = function(models) {
-    models.Example.belongsTo(models.User, {
-      onDelete: "CASCADE",
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
-  return Example;
-};
+//   Example.associate = function(models) {
+//     models.Example.belongsTo(models.User, {
+//       onDelete: "CASCADE",
+//       foreignKey: {
+//         allowNull: false
+//       }
+//     });
+//   };
+//   return Example;
+// };
