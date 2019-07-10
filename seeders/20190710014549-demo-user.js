bd4231d614ae88da96b591f2991a1900b2f60635
Oakkar <oakkaraung1659@gmail.com>
2019-07-10 13:40:20 -0700

    updated

--- seeders/20190710014549-demo-user.js
+++ seeders/20190710014549-demo-user.js
@@ -1,12 +1,9 @@
 "use strict";
-<<<<<<< HEAD
-=======
 var faker = require("faker");
 // paste this into terminal to seed ALL seed files (must be in seperate instance of terminal than the server running)
 //          $ npx sequelize-cli db:seed:all
 // also it may give an error if you haven't exported the line from your env into terminal:
 //          export DEV_MYSQL_URL=...
->>>>>>> 8d7e6ece21a19a7b90aa863b3fa76cb9cbfbb066
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -23,34 +20,6 @@ module.exports = {
     // let now = new Date();
     // let nowTime = now.toISOString();
     let nowTime = "2019-07-10 02:43:00";
-<<<<<<< HEAD
-    let dummyUsers = [
-      {
-        email: "Ross@dummy.com",
-        password: "rossisboss",
-        createdAt: nowTime,
-        updatedAt: nowTime
-      },
-      {
-        email: "Lisa@dummy.com",
-        password: "lisathepizza",
-        createdAt: nowTime,
-        updatedAt: nowTime
-      },
-      {
-        email: "David@dummy.com",
-        password: "daveismade",
-        createdAt: nowTime,
-        updatedAt: nowTime
-      },
-      {
-        email: "Susan@dummy.com",
-        password: "susansnoman",
-        createdAt: nowTime,
-        updatedAt: nowTime
-      }
-    ];
-=======
     let dummyUsers = [];
     for (let i = 0; i < 50; i++) {
       let newUser = {
@@ -83,7 +52,6 @@ module.exports = {
     //   createdAt: nowTime,
     //   updatedAt: nowTime
     // }]
->>>>>>> 8d7e6ece21a19a7b90aa863b3fa76cb9cbfbb066
 
     return queryInterface.bulkInsert("Users", dummyUsers, {});
   },
